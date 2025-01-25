14e5ba0d59629b7295eb7002752480e874f71fdf
FriendShire <mineminepav@gmail.com>
2025-01-25 22:14:59 +0200

    FAQ updated

--- Scripts/JSall.js
+++ Scripts/JSall.js
@@ -2,37 +2,37 @@ const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 if (isTouchDevice) {
     console.log("Плавная прокрутка отключена");
 } else {
-    document.addEventListener('DOMContentLoaded', () => {
-        console.log("Плавная прокрутка включена");
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     console.log("Плавная прокрутка включена");
 
-        // -----------------------
-        // Плавная прокрутка 
-        const smoothCoef = 0.05;
-        const smoothScroll = document.querySelector(".smooth-scroll");
-        const smoothScrollBar = document.querySelector(".smooth-scrollbar");
+    //     // -----------------------
+    //     // Плавная прокрутка 
+    //     const smoothCoef = 0.05;
+    //     const smoothScroll = document.querySelector(".smooth-scroll");
+    //     const smoothScrollBar = document.querySelector(".smooth-scrollbar");
 
-        function onResize(e) {
-            smoothScrollBar.style.height = smoothScroll.offsetHeight + "px";
-        }
+    //     function onResize(e) {
+    //         smoothScrollBar.style.height = smoothScroll.offsetHeight + "px";
+    //     }
 
-        window.addEventListener("resize", onResize);
-        onResize();
+    //     window.addEventListener("resize", onResize);
+    //     onResize();
 
-        let prevY = window.scrollY;
-        let curY = window.scrollY;
-        let y = window.scrollY;
-        let dy;
+    //     let prevY = window.scrollY;
+    //     let curY = window.scrollY;
+    //     let y = window.scrollY;
+    //     let dy;
 
-        function loop(now) {
-            curY = window.scrollY;
-            dy = curY - prevY;
-            y = Math.abs(dy) < 1 ? curY : y + dy * smoothCoef;
-            prevY = y;
-            smoothScroll.style.transform = `translate3d(0,${-y}px,0)`;
+    //     function loop(now) {
+    //         curY = window.scrollY;
+    //         dy = curY - prevY;
+    //         y = Math.abs(dy) < 1 ? curY : y + dy * smoothCoef;
+    //         prevY = y;
+    //         smoothScroll.style.transform = `translate3d(0,${-y}px,0)`;
 
-            requestAnimationFrame(loop);
-        }
-        requestAnimationFrame(loop);
-            });
+    //         requestAnimationFrame(loop);
+    //     }
+    //     requestAnimationFrame(loop);
+    //         });
 
-}  
+}
